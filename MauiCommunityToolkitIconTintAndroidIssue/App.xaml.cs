1f593e2d8871c45fda2ed702ce93afefe3079ce6
samueljames <samuel.james@bt.com>
2023-11-29 15:19:52 +0000

    init

---
+++ MauiCommunityToolkitIconTintAndroidIssue/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiCommunityToolkitIconTintAndroidIssue;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiCommunityToolkitIconTintAndroidIssue/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Maui.Behaviors;
+
+namespace MauiCommunityToolkitIconTintAndroidIssue;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+        Image myImage = new()
+        {
+            BackgroundColor = Colors.SeaGreen,
+            Source = "dotnet_bot.png",
+            HeightRequest = 185,
+            Aspect = Aspect.AspectFit
+        };
+
+        Image myBrokenImage = new()
+        {
+            BackgroundColor = Colors.Yellow,
+            Source = "dotnet_bot.png",
+            HeightRequest = 185,
+            Aspect = Aspect.AspectFit
+        };
+
+        myImage.Behaviors.Add(new IconTintColorBehavior()
+        {
+            TintColor = Colors.Black
+        });
+
+        myBrokenImage.Behaviors.Add(new IconTintColorBehavior()
+        {
+            TintColor = null
+        });
+
+        VerticalStackLayout myStack = new()
+        {
+            myImage,
+            myBrokenImage
+        };
+
+        ScrollView scrollView = new()
+        {
+            Content = myStack
+        };
+
+        this.Content = scrollView;
+	}
+}
